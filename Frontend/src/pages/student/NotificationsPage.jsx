4ce4239072e101b3568b5ebdbb9ffc55eeaa6e4a
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2026-05-05 04:28:38 +0530

    Notification  page looks Awsome version 7 looks clean and minimal

--- Frontend/src/pages/student/NotificationsPage.jsx
+++ Frontend/src/pages/student/NotificationsPage.jsx
@@ -124,9 +124,9 @@ const NotificationsPage = () => {
           <button 
             onClick={handleMarkAllAsRead}
             className="text-sm font-medium 
-            bg-slate-900 text-white 
-            border border-slate-900 
-            hover:bg-slate-800 
+            bg-white text-slate-700 
+            border border-slate-200 
+            hover:bg-slate-100 hover:text-slate-900
             px-4 py-2 rounded-xl 
             shadow-sm hover:shadow-md 
             hover:-translate-y-[1px] 
@@ -147,7 +147,7 @@ const NotificationsPage = () => {
           hover:-translate-y-[1px]">
             
           {/* ICON */}
-          <div className="w-10 h-10 bg-slate-900 text-white shadow-sm rounded-xl flex items-center justify-center shadow-sm shrink-0">
+          <div className="w-10 h-10 bg-slate-100 text-slate-600 rounded-xl flex items-center justify-center shrink-0">
 
             <Bell size={20} />
           </div>
@@ -167,7 +167,7 @@ const NotificationsPage = () => {
           hover:-translate-y-[1px]">
 
           {/* ICON */}
-          <div className="w-10 h-10 bg-slate-900 text-white shadow-sm rounded-xl flex items-center justify-center shadow-sm shrink-0 relative">
+          <div className="w-10 h-10 bg-slate-100 text-slate-600 rounded-xl flex items-center justify-center shrink-0">
              <MailOpen size={20} />
              {stats.unread > 0 && <span className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full border-2 border-white"></span>}
           </div>
@@ -187,7 +187,7 @@ const NotificationsPage = () => {
           hover:-translate-y-[1px]">
           
           {/* ICON */}
-          <div className="w-10 h-10 bg-slate-900 text-white shadow-sm rounded-xl flex items-center justify-center shadow-sm shrink-0">
+          <div className="w-10 h-10 bg-slate-100 text-slate-600 rounded-xl flex items-center justify-center shrink-0">
              <Check size={20} />
           </div>
           
@@ -205,7 +205,7 @@ const NotificationsPage = () => {
           hover:border-slate-300 
           hover:-translate-y-[1px]">
           {/* ICON */}
-          <div className="w-10 h-10 bg-slate-900 text-white shadow-sm rounded-xl flex items-center justify-center shadow-sm shrink-0">
+          <div className="w-10 h-10 bg-slate-100 text-slate-600 rounded-xl flex items-center justify-center shrink-0">
              <AlertTriangle size={20} />
           </div>
 
